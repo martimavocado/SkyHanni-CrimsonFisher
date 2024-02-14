31f53403f13a5922d9c2cdecfcd20b3232f80133
martimavocado <martim.cavaco@tutanota.com>
2024-02-14 23:19:47 +0000

    i swear intellij keeps turning off auto optimize

--- src/main/java/at/hannibal2/skyhanni/features/misc/EnderPearlCooldown.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/EnderPearlCooldown.kt
@@ -2,18 +2,12 @@ package at.hannibal2.skyhanni.features.misc
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.data.ClickType
-import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.events.WorldClickEvent
 import at.hannibal2.skyhanni.utils.ChatUtils
-import at.hannibal2.skyhanni.utils.ItemCategory
 import at.hannibal2.skyhanni.utils.ItemUtils.getInternalName
-import at.hannibal2.skyhanni.utils.ItemUtils.getItemCategoryOrNull
-import at.hannibal2.skyhanni.utils.LorenzUtils
-import at.hannibal2.skyhanni.utils.LorenzUtils.isInIsland
 import at.hannibal2.skyhanni.utils.NEUInternalName.Companion.asInternalName
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import tv.twitch.chat.Chat
 import kotlin.time.Duration.Companion.seconds
 
 class EnderPearlCooldown {
